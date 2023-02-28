b8afc1de7c3703edf98b176fbcbbff0e1741b7df
Bryan Rodriguez <bryan.j.rodriguez@intel.com>
2023-02-27 23:11:23 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ $(id -u) -ne 0 ]]; then
-    echo -e "\e[1m\e[31;1m Please run this script as root \e[0m"
+    echo -e "\e[1m\e[31;1m Please run this script as root or with sudo \e[0m"
     exit 1
 fi
 
 mkdir -p /opt/esp && \
 cd /opt/esp && \
-wget -O /opt/esp/docker-compose.yml https://github.com/myuser/esp/raw/branch/master/docker-compose.yml && \
+wget -O /opt/esp/docker-compose.yml https://raw.githubusercontent.com/brod-intel/esp/main/docker-compose.yml && \
 docker-compose up -d
