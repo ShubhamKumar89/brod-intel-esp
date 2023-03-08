ce53d8335b6fbca5150bf22955f66e67232b7bc2
Bryan J Rodriguez <bryan.j.rodriguez@intel.com>
2023-03-08 15:37:57 -0700

    added installing docker-compose

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,9 @@ if ! command -v docker >/dev/null 2>&1; then
 fi
 
 if ! command -v docker-compose >/dev/null 2>&1; then
-    echo -e "\e[1m\e[31;1m Please install docker-compose! \e[0m"
-    exit 1
+    echo -e "\e[1m\e[31;1m Installing docker-compose.! \e[0m"
+    wget -O /usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" && \
+    chmod a+x /usr/local/bin/docker-compose
 fi
 
 mkdir -p /opt/esp && \
