784a875bcea8fc53d50000fefc010c077518ee52
Bryan J Rodriguez <bryan.j.rodriguez@intel.com>
2023-03-08 14:48:27 -0700

    added detection

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,16 @@ if [[ $(id -u) -ne 0 ]]; then
     exit 1
 fi
 
+if ! command -v <docker> &> /dev/null; then
+    echo -e "\e[1m\e[31;1m Please install docker! \e[0m"
+    exit 1
+fi
+
+if ! command -v <docker-compose> &> /dev/null; then
+    echo -e "\e[1m\e[31;1m Please install docker-compose! \e[0m"
+    exit 1
+fi
+
 mkdir -p /opt/esp && \
 cd /opt/esp && \
 wget -O /opt/esp/docker-compose.yml https://raw.githubusercontent.com/brod-intel/esp/main/docker-compose.yml && \
